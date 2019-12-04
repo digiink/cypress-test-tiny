75d21c7e38f89b14ae98ea6bd5e353856343246d
Brendan McGinn <brendan.mcginn@avbrowne.com>
2019-12-04 09:33:45 +0000

    screens over 400px width fail

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,11 @@
 describe('page', () => {
-  it('works', () => {})
+  it('works', () => {
+    cy.viewport(4000, 1601);
+  })
+})
+
+describe('large_page', () => {
+  it('fails', () => {
+    cy.viewport(7680, 1601);
+  })
 })
